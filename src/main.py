ccd23261f064bafa4d44f7eca45cab732e1199b6
kdhok7 <kdhok7@naver.com>
2026-06-16 17:33:09 +0900

    과제 제출합니다.

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ import numpy as np
 #
 # OpenCV 함수를 활용하세요.
 def convert_to_gray(image):
-    raise NotImplementedError
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
 # 문제 2.
@@ -22,7 +22,7 @@ def resize_image(
     width,
     height,
 ):
-    raise NotImplementedError
+    return cv2.resize(image, (width, height))
 
 
 # 문제 3.
@@ -37,7 +37,8 @@ def apply_threshold(
     gray_image,
     threshold_value,
 ):
-    raise NotImplementedError
+    _, binary = cv2.threshold(gray_image, threshold_value, 255, cv2.THRESH_BINARY)
+    return binary
 
 
 # 문제 4.
@@ -53,7 +54,13 @@ def apply_threshold(
 #
 # 조건에 맞는 픽셀 개수를 반환하세요.
 def count_red_pixels(image):
-    raise NotImplementedError
+    b = image[:, :, 0]
+    g = image[:, :, 1]
+    r = image[:, :, 2]
+    
+    red_mask = (r > 200) & (g < 50) & (b < 50)
+    
+    return int(np.sum(red_mask))
 
 
 # 문제 5.
@@ -68,4 +75,12 @@ def count_red_pixels(image):
 #
 # 객체가 없으면 None 반환
 def find_object_center(mask):
-    raise NotImplementedError
+    y_indices, x_indices = np.where(mask == 255)
+    
+    if len(y_indices) == 0:
+        return None
+        
+    cx = int(np.mean(x_indices))
+    cy = int(np.mean(y_indices))
+    
+    return (cx, cy)
